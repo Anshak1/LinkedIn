81874e435f6f6685e743515b9420fdbdcf471588
Sk Md Jeesan <skmdjeesan@gmail.com>
2026-06-22 17:32:50 +0530

    post card bugs fix

--- server/src/models/post.model.js
+++ server/src/models/post.model.js
@@ -1,11 +1,16 @@
 import mongoose from "mongoose";
 
+const commentSchema = new mongoose.Schema({
+  content: { type: String, required: true },
+  user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true }
+}, { timestamps: true });
+
 const postSchema = new mongoose.Schema({
   author: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
   description: {type: String, default: ''},
-  image: {type: String,},
+  image: {type: String},
   like: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
-  comment: [{content: String, user: {type: mongoose.Schema.Types.ObjectId, ref: 'User'}}]
+  comment: [commentSchema]
 }, {timestamps: true})
 
 const Post = mongoose.model('Post', postSchema)
